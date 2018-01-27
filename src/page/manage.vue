c7dfd5a880877794033d54b3c61b9294727e9959
zhzhwwqq <zhzhwwqq@sina.com>
2018-01-27 09:31:58 +0800

    first

---
+++ src/page/manage.vue
@@ -0,0 +1,79 @@
+<template>
+	<!-- <div class="manage_page fillcontain">
+		<el-row style="height: 100%;">
+	  		<el-col :span="4"  style="min-height: 100%; background-color: #324057;"> -->
+				  
+<el-container>
+	<el-aside width="200px">
+				<el-menu :default-active="defaultActive" style="min-height: 100%;" background-color="rgb(29,43,54)"
+                    text-color="#fff"
+                    active-text-color="#ffd04b" router>
+					<el-menu-item index="manage"><i class="el-icon-menu"></i>首页</el-menu-item>
+					<el-submenu index="2">
+						<template slot="title"><i class="el-icon-document"></i>数据管理</template>
+						<el-menu-item index="userList">用户列表</el-menu-item>
+						<el-menu-item index="shopList">商家列表</el-menu-item>
+						<el-menu-item index="foodList">食品列表</el-menu-item>
+						<el-menu-item index="orderList">订单列表</el-menu-item>
+						<el-menu-item index="adminList">管理员列表</el-menu-item>
+					</el-submenu>
+					<el-submenu index="3">
+						<template slot="title"><i class="el-icon-plus"></i>添加数据</template>
+						<el-menu-item index="addShop">添加商铺</el-menu-item>
+						<el-menu-item index="addGoods">添加商品</el-menu-item>
+					</el-submenu>
+					<el-submenu index="4">
+						<template slot="title"><i class="el-icon-star-on"></i>图表</template>
+						<el-menu-item index="visitor">用户分布</el-menu-item>
+						<!-- <el-menu-item index="newMember">用户数据</el-menu-item> -->
+					</el-submenu>
+					<el-submenu index="5">
+						<template slot="title"><i class="el-icon-edit"></i>编辑</template>
+						<!-- <el-menu-item index="uploadImg">上传图片</el-menu-item> -->
+						<el-menu-item index="vueEdit">文本编辑</el-menu-item>
+					</el-submenu>
+					<el-submenu index="6">
+						<template slot="title"><i class="el-icon-setting"></i>设置</template>
+						<el-menu-item index="adminSet">管理员设置</el-menu-item>
+						<!-- <el-menu-item index="sendMessage">发送通知</el-menu-item> -->
+					</el-submenu>
+					<el-submenu index="7">
+						<template slot="title"><i class="el-icon-warning"></i>说明</template>
+						<el-menu-item index="explain">说明</el-menu-item>
+					</el-submenu>
+					<el-submenu index="8">
+						<template slot="title"><i class="el-icon-warning"></i>多标签页</template>
+						<el-menu-item index="multiTabs">多标签页</el-menu-item>
+					</el-submenu>
+				</el-menu>
+			<!-- </el-col> -->
+			</el-aside>
+			<!-- <el-col :span="20" style="height: 100%;overflow: auto;"> -->
+				 <el-main>
+				<keep-alive>
+				    <router-view></router-view>
+				</keep-alive>
+				</el-main>
+			<!-- </el-col> -->
+			</el-container>
+		<!-- </el-row>
+  	</div> -->
+</template>
+
+<script>
+    export default {
+		computed: {
+			defaultActive: function(){
+				return this.$route.path.replace('/', '');
+			}
+		},
+    }
+</script>
+
+
+<style lang="less" scoped>
+	@import '../style/mixin';
+	.manage_page{
+		
+	}
+</style>
---
+++ src/page/multiTabs.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+        <el-tabs type="border-card" v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="用户管理" name="first">
+          <user-tab></user-tab>
+        </el-tab-pane>
+        <el-tab-pane label="配置管理" name="second">
+          <order-tab></order-tab>
+        </el-tab-pane>
+        <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+        <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+        </el-tabs>
+  </div>
+</template>
+<script>
+  import userTab from '../components/userTab'
+  import orderTab from '../components/orderTab' 
+  export default {
+    data() {
+      return {
+        activeName: 'second'
+      };
+    },
+    components:{
+      userTab,
+      orderTab
+    },
+    methods: {
+      handleClick(tab, event) {
+        console.log(tab, event);
+        
+      }
+    },	
+  };
+</script>
